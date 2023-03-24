07ea682162fca8cb6f23764289532c9c1c5b7e5f
m1pan <michaelpan2002@gmail.com>
2023-03-24 09:27:23 +0000

    final a

--- finaltest/a.py
+++ finaltest/a.py
@@ -6,3 +6,36 @@ import random as rn
 cid = [0,1,8,5,3,1,8,9]
 
 #Task A
+
+#define a factorial function
+def factorial(n):
+    if n <= 1:
+        return 1
+    sum = n * factorial(n-1)
+    return sum
+
+#define function series to take in n and output s
+def Series(N):
+    #initialise S
+    S = 0
+    #sum from -N to N+2
+    for j in range(-N,N+3):
+        #initialise sub sum
+        sub = 0
+        #sub sum from k=2 to 10j
+        for k in range(2,(10*j)+1,2):
+            sub += ((-1)**j)*(j**k)/factorial(k)
+        S += sub/factorial(j)
+    return S
+
+
+#initialise empty list S
+S = []
+
+#compute sum 8 times with cid and fill up S
+for i in range(8):
+    S.append(Series(cid[i]))
+
+plt.plot(range(8),S)
+plt.show()
+
