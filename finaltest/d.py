e4f1e7cb06589931d66dca50e5562e4c3bde7a88
m1pan <michaelpan2002@gmail.com>
2023-03-24 10:21:29 +0000

    final d

--- finaltest/d.py
+++ finaltest/d.py
@@ -6,3 +6,18 @@ import random as rn
 cid = [0,1,8,5,3,1,8,9]
 
 #Task D
+
+#define recrusive function sequence
+def Sequence(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        y = Sequence(n-1) - (n+1)*Sequence(n-2)
+        return y
+
+#print first 20 values of sequence
+print([Sequence(i) for i in range(1,21)])
+
+    
